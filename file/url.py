0bbce0c599c2548c6630fedfb4b3063d8ba9ecac
letyougo <313755017@qq.com>
2016-11-21 19:29:24 +0800

    all the code
    Signed-off-by: letyougo <313755017@qq.com>

--- file/url.py
+++ file/url.py
@@ -10,6 +10,10 @@ urlpatterns = patterns('',
     url(r'^file/rename/$', views.rename),
     url(r'^file/mkdir/$', views.mkdir),
     url(r'^file/remove/$', views.remove),
+    url(r'^file/copy/$', views.copy),
+    url(r'^file/move/$', views.cut),
+    url(r'^file/upload/$', views.upload),
+
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- file/views.py
+++ file/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,24 +9,27 @@ from dongnaoApi.settings import FILE_ROOT,FILE_SYSTEM_DIR
 import os
 import shutil
 
-
+    
 def filter(path):
-    if path == '':
-       path = '/'
-    if path[0] == '/':
-	path = path[1:]
-    return os.path.join(FILE_SYSTEM_DIR,path)
+   if not path:
+      path = '/'
+   if path == '':
+	path = '/'
+   if path[0] == '/':
+       path = path[1:]
+   return os.path.join(FILE_SYSTEM_DIR,path)
 
 
 def get(request):
     path = request.GET['path']
-    p = filter(path)	
+    p = filter(path)
     info = os.listdir(p)
 
     json = []
     for i in info:
         d = {}
-        d['path'] = os.path.join(path,i)
+	print path,i,'hello'
+	d['path'] = os.path.join(path,i)
         d['name'] = i
         d['ext'] = os.path.splitext(i)[1]
         d['isFolder'] = os.path.isdir(filter(d['path']))
@@ -60,6 +64,7 @@ def rename(request):
 def mkdir(request):
     path = request.GET['path']
     path = filter(path)
+    
     name = request.GET['name']
 
     dist = os.path.join(path,name)
@@ -75,7 +80,61 @@ def mkdir(request):
 	ext=os.path.splitext(name)[1],
 	isFolder=os.path.isdir(dist)
     ))
+def copy(request):
+    old_path = request.GET['old_path']
+    old_path = filter(old_path)
+    new_path = request.GET['new_path']
+    new_path = filter(new_path)
 
+    if os.path.exists(new_path):
+        return JsonResponse(dict(
+                error='file exists'
+            ))
+
+    if not os.path.isdir(old_path):
+	fn = shutil.copyfile
+    else:
+	fn = shutil.copytree
+
+    fn(old_path,new_path)
+    p = new_path.split('/')
+    name = p[len(p)-1]
+    return JsonResponse(dict(
+	path = new_path.replace('/home/student/static/',''),
+	name = name,
+	ext=os.path.splitext(new_path)[1],
+	isFolder = os.path.isdir(new_path)
+))
+
+def cut(request):
+    old_path = request.GET['old_path']
+    old_path = filter(old_path)
+    new_path = request.GET['new_path']
+    new_path = filter(new_path)
+
+    if os.path.exists(new_path):
+        return JsonResponse(dict(
+                error='file exists'
+            ))
+
+    if not os.path.isdir(old_path):
+	copy = shutil.copyfile
+        rm = os.remove
+    else:
+	copy = shutil.copytree
+        rm = shutil.rmtree
+
+    copy(old_path,new_path)
+    rm(old_path)
+    p = new_path.split('/')
+    name = p[len(p)-1]
+    return JsonResponse(dict(
+	path = new_path.replace('/home/student/static/',''),
+	name = name,
+	ext=os.path.splitext(new_path)[1],
+	isFolder = os.path.isdir(new_path)
+))
+    
 def remove(request):
     path = request.GET['path']
     path = filter(path)
@@ -90,3 +149,23 @@ def remove(request):
     return JsonResponse(dict(
         success='delete files successful'
     ))
+
+def upload(request):
+    files = request.FILES.getlist('cloud')
+    print files
+    p = filter(request.POST['path'])
+    print p
+    print files
+    for f in files:
+        print f   
+        
+        target = open(p+f.name,'wb+')
+        if f.multiple_chunks():
+            for chunk in f.chunks():
+                target.write(chunk)
+        else:
+            chunk = f.read()
+            target.write(chunk)
+        target.close()
+
+    return HttpResponse('hello world')
